4864c60e374b856eb84ed23d7a822925ae7b5c4f
cutebid <cutebid1993@gmail.com>
2024-09-22 08:44:45 +0100

    resposiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
           </ul>
         </div>
       </div>
-      <div className="mt-32">
+      <div className="mt-36">
       <CustomeButton btnTitle="Get started" onClick={() => router.push('/getStarted')} />
       </div>
       
