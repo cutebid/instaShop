01c93c98ac17fce1de0dee8f0258d6d47bc4ad9e
cutebid <cutebid1993@gmail.com>
2024-09-22 10:16:33 +0100

    resposiveness

--- src/app/components/Accordion.tsx
+++ src/app/components/Accordion.tsx
@@ -9,7 +9,12 @@ interface AccordionProps {
   children: React.ReactNode;
 }
 
-const Accordion: React.FC<AccordionProps> = ({ title, isOpen, onClick, children }) => {
+const Accordion: React.FC<AccordionProps> = ({
+  title,
+  isOpen,
+  onClick,
+  children,
+}) => {
   return (
     <div className="border-y border-gray-200 pt-6 pb-4 px-4 ">
       <div
@@ -17,22 +22,25 @@ const Accordion: React.FC<AccordionProps> = ({ title, isOpen, onClick, children 
         onClick={onClick}
       >
         <h3 className="font-medium text-sm">{title}</h3>
-        <div> <GoChevronDown size={24} color="gray" /></div>
+        <div>
+          <GoChevronDown size={24} color="gray" />
+        </div>
       </div>
-      
-        <AnimatePresence>
+
+      <AnimatePresence>
         {isOpen && (
-        <motion.div initial={{ opacity: 0, y: -10 }}
-        animate={{ opacity: 1, y: 10}}
-        exit={{ opacity: 0, y: -10 }}
-        transition={{ duration: 0.6 }} className="mt-4">
-          {children}
-        </motion.div>
-      )}
-        </AnimatePresence>
-      </div>
-     
-    
+          <motion.div
+            initial={{ opacity: 0, y: -10 }}
+            animate={{ opacity: 1, y: 10 }}
+            exit={{ opacity: 0, y: -10 }}
+            transition={{ duration: 0.6 }}
+            className="mt-4"
+          >
+            {children}
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
   );
 };
 
